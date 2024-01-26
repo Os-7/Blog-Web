1a5a3f820cea54720219227f77455f640efebedc
Omshree Samal <96040535+Os-7@users.noreply.github.com>
2024-01-26 23:34:43 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -4,7 +4,7 @@ import { API_NOTIFICATION_MESSAGES, SERVICE_URLS } from '../constants/config';
 import { getAccessToken, getType } from '../utils/common-utils';
 
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://blog-backend-jsrx.onrender.com';
 
 
 const axiosInstance = axios.create({
